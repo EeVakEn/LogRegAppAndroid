bece96be3b7d330334e27efafedbd6fc2c69d73c
Ivakhin <danucha129@gmail.com>
2020-09-23 00:16:15 +0700

    Registration disign and validation

--- src/main/java/com/eevaken/logregapp/MainActivity.java
+++ src/main/java/com/eevaken/logregapp/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends Activity {
     Button regButton;
     EditText name;
@@ -16,6 +18,13 @@ public class MainActivity extends Activity {
     EditText email;
     EditText username;
     EditText password;
+    TextView errorMsg;
+    String errorStr;
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String PASSWORD_PATTERN =
+            "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +32,57 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         regButton = (Button) findViewById(R.id.regRegistrationButton);
-        name = (EditText) findViewById(R.id.regEditTextName);
-        surname =(EditText) findViewById(R.id.regEditTextSurname);
-        email = (EditText) findViewById(R.id.regEditTextEmail);
-        username = (EditText) findViewById(R.id.regEditTextUsername);
-        password = (EditText) findViewById(R.id.regEditTextPassword);
 
-        View.OnClickListener oclBtn = new View.OnClickListener() {
+        errorMsg = (TextView) findViewById(R.id.regTextViewErrorMsg);
+        View.OnClickListener oclBtn;
+        oclBtn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    username.setText("ХУЙ");
-                    name.setText("ХУЙ");
-                    surname.setText("ПИЗДА");
+                name = (EditText) findViewById(R.id.regEditTextName);
+                surname = (EditText) findViewById(R.id.regEditTextSurname);
+                email = (EditText) findViewById(R.id.regEditTextEmail);
+                username = (EditText) findViewById(R.id.regEditTextUsername);
+                password = (EditText) findViewById(R.id.regEditTextPassword);
+
+                if (!validation(name.getText().toString(), surname.getText().toString(),
+                        email.getText().toString(), username.getText().toString(),
+                        password.getText().toString()))
+                    errorMsg.setText(errorStr);
+                else
+                    errorMsg.setText("");
             }
         };
 
         regButton.setOnClickListener(oclBtn);
+    }
+
+    private boolean validation(String nameStr, String surnameStr,
+                              String emailStr, String usernameSrt, String passwordStr) {
+        if (nameStr.equals("")) {
+            errorStr = "Empty name";
+            return false;
+        }
+        if (surnameStr.equals("")) {
+            errorStr = "Empty surname";
+            return false;
+        }
+        if (emailStr.equals("") || !emailStr.matches(EMAIL_PATTERN)) {
+            errorStr = "Email is invalid";
+            return false;
+        }
+        if (usernameSrt.equals("")) {
+            errorStr ="Empty username";
+            return false;
+        }
+        System.out.println(passwordStr);
+        if (passwordStr.equals("") || !passwordStr.matches(PASSWORD_PATTERN)) {
+            errorStr = "Password is invalid. Check:\n" +
+                    " At least 8 characters\n" +
+                    "Contains at least one digit\n" +
+                    "Contains at least one lower alpha char \n" +
+                    "and one upper alpha char";
+            return false;
+        }
+        return true;
     }
 }
